b02c68ff7ead6e5d448d26634f13d70ecb27fbba
DanSBol <dan.s.boltachev@yandex.ru>
2024-08-06 17:32:09 +0300

    fix: диплом (часть 1 из 3) - учел замечания ревьювера - кроме PersistenceConfig (детали под замечанием)

--- stats/stats-dto/src/main/java/ru/practicum/ewm/dto/stats/StatsDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/ewm/dto/stats/StatsDto.java
@@ -3,13 +3,9 @@ package ru.practicum.ewm.dto.stats;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Data
-@Getter
-@Setter
 @Builder(builderClassName = "StatsDtoBuilder")
 @AllArgsConstructor
 @NoArgsConstructor
--- stats/stats-server/src/main/java/ru/practicum/ewm/stats/StatsServiceImpl.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/stats/StatsServiceImpl.java
@@ -13,7 +13,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@Transactional
 public class StatsServiceImpl implements StatsService {
     private final StatsRepository statsRepository;
 
