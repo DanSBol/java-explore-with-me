eb1b44bb345dcaededd04ce4ac8ff4e58a5de775
DanSBol <dan.s.boltachev@yandex.ru>
2024-08-20 17:56:55 +0300

    feat: диплом - часть 2 из 3 (main сервис) - часть 3 из 3 (доп. функционал рейтингов)

---
+++ ewm-service/src/main/java/ru/practicum/explore/main/category/dto/CategoryDto.java
@@ -0,0 +1,12 @@
+package ru.practicum.explore.main.category.dto;
+
+import lombok.Data;
+
+import jakarta.validation.constraints.Size;
+
+@Data
+public class CategoryDto {
+    private Long id;
+    @Size(max = 50, message = "Количество символов в имени категории должно быть не больше 50")
+    private String name;
+}
